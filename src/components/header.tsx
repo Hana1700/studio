a46fbc1598b95cdb1eae41831e06629821e2f617
Hana Ben <hanaben1709@gmail.com>
2025-10-06 08:06:09 +0000

    je parle lorsque on agrandit la page

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,8 +8,9 @@ export function AppHeader() {
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-16 items-center">
-        <div className="mr-4 hidden md:flex items-center">
-          <Link href="/" className="flex items-center space-x-2">
+        <div className="mr-4 md:flex items-center">
+           <SidebarTrigger className="md:hidden mr-2" />
+          <Link href="/" className="hidden md:flex items-center space-x-2">
             <AppLogo className="h-6 w-6 text-primary" />
             <span className="hidden font-bold font-headline sm:inline-block">
               Annuaire
@@ -17,22 +18,16 @@ export function AppHeader() {
           </Link>
         </div>
 
-        <div className='md:hidden'>
-           <SidebarTrigger className="md:hidden mr-2" />
-        </div>
-
         <div className="flex-1 flex justify-center px-4">
-            <div className="w-full max-w-md">
-              <SearchBar />
-            </div>
+          <div className="w-full max-w-md">
+            <SearchBar />
+          </div>
         </div>
 
         <div className="flex items-center justify-end space-x-4">
-           <Link href="/" className="flex items-center space-x-2">
-            <span className="hidden font-bold font-headline sm:inline-block">
-              Annuaire téléphonique de......
-            </span>
-          </Link>
+          <span className="hidden font-bold font-headline sm:inline-block">
+            Annuaire téléphonique de......
+          </span>
           <nav className="flex items-center">
             <Button variant="ghost" asChild>
               <Link href="/admin">Admin</Link>
